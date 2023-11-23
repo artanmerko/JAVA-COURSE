3884c4cbf978ea25f395d297648d1d2d9ced31b9
artanmerko <artanmerko@gmail.com>
2023-11-23 17:45:33 -0500

    OrderItems

--- Java Fundamentals/OrdersItem/src/TestOrders.java
+++ Java Fundamentals/OrdersItem/src/TestOrders.java
@@ -19,7 +19,47 @@ public class TestOrders {
             item4.name = "capuccino";
             item4.price = 2.75;
 
-            
+            Order order1 = new Order();
+            System.out.println(order1.total);
+            order1.name = "Cindhuri";
+
+            Order order2 = new Order();
+            order2.name = "Jimmy";
+
+            Order order3 = new Order();
+            order3.name = "Noah";
+
+            Order order4 = new Order();
+            order4.name = "Sam";
+
+            order2.items.add(item1);
+            order2.total = order2.total + item1.price;
+
+            order3.items.add(item2);
+            order3.total = order3.total + item2.price;
+
+            order4.items.add(item2);
+            order4.items.add(item2);
+            order4.total = order4.total + item2.price * 2;
+
+            order1.ready = true;
+            order2.ready = true;
+
+            System.out.printf("Name: %s\n", order1.name);
+            System.out.printf("Total: %s\n", order1.total);
+            System.out.printf("Ready: %s\n", order1.ready);
+
+            System.out.printf("Name: %s\n", order2.name);
+            System.out.printf("Total: %s\n", order2.total);
+            System.out.printf("Ready: %s\n", order2.ready);
+
+            System.out.printf("Name: %s\n", order3.name);
+            System.out.printf("Total: %s\n", order3.total);
+            System.out.printf("Ready: %s\n", order3.ready);
+
+            System.out.printf("Name: %s\n", order4.name);
+            System.out.printf("Total: %s\n", order4.total);
+            System.out.printf("Ready: %s\n", order4.ready);
 
     }
 
