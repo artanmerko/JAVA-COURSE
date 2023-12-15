60fde3dbc445495539020a0e8c9262c2b953f470
artanmerko <artanmerko@gmail.com>
2023-12-15 16:30:02 -0500

    book club

---
+++ Java Spring/Book Club/src/main/java/com/codingdojo/bookclub/controllers/HomeController.java
@@ -0,0 +1,130 @@
+package com.codingdojo.bookclub.controllers;
+
+import com.codingdojo.bookclub.models.Book;
+import com.codingdojo.bookclub.models.LoginUser;
+import com.codingdojo.bookclub.models.User;
+import com.codingdojo.bookclub.services.BookService;
+import com.codingdojo.bookclub.services.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    private UserService users;
+    @Autowired
+    private BookService books;
+
+    @GetMapping("/")
+    public String index(Model model, @ModelAttribute("newUser") User newUser,
+                        @ModelAttribute("newLogin") User newLogin, HttpSession session) {
+        if(session.getAttribute("userId") != null) {
+            return "redirect:/home";
+        }
+        model.addAttribute("newUser", new User());
+        model.addAttribute("newLogin", new LoginUser());
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String register(@Valid @ModelAttribute("newUser") User newUser, BindingResult result, Model model, HttpSession session) {
+
+        User user = users.register(newUser, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newLogin", new LoginUser());
+            return "index";
+        }
+        session.setAttribute("userId", user.getId());
+        return "redirect:/home";
+    }
+
+    @PostMapping("/login")
+    public String login(@Valid @ModelAttribute("newLogin") LoginUser newLogin, BindingResult result, Model model, HttpSession session) {
+
+        User user = users.login(newLogin, result);
+        if(result.hasErrors()) {
+            model.addAttribute("newUser", new User());
+            return "index";
+        }
+        session.setAttribute("userId", user.getId());
+        return "redirect:/home";
+    }
+
+    @GetMapping("/home")
+    public String home(Model model, HttpSession session) {
+        if(session.getAttribute("userId") == null) {
+            return "redirect:/";
+        }
+        model.addAttribute("books", books.all());
+        model.addAttribute("user", users.findById((Long)session.getAttribute("userId")));
+        return "home";
+    }
+
+    @GetMapping("/addPage")
+    public String addPage(@ModelAttribute("book") Book book, Model model, HttpSession session) {
+        User user = users.findById((Long)session.getAttribute("userId"));
+        model.addAttribute("user", user);
+        return "addPage";
+    }
+
+    @PostMapping("/books")
+    public String createBook(@Valid @ModelAttribute("book") Book book, BindingResult result) {
+        if (result.hasErrors()) {
+            return "addPage";
+        }
+        books.create(book);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/books/{id}")
+    public String bookDetail(Model model, @PathVariable("id") Long id, HttpSession session) {
+        if(session.getAttribute("userId") == null) {
+            return "redirect:/home";
+        }
+        Book book = books.findById(id);
+        model.addAttribute("book", book);
+        model.addAttribute("user", users.findById((Long)session.getAttribute("userId")));
+        return "book";
+    }
+
+    @GetMapping("/books/{id}/edit")
+    public String getEditBook(@PathVariable("id") Long id, Model model,HttpSession session){
+        if(session.getAttribute("userId") == null) {
+            return "redirect:/home";
+        }
+        Book bookEdit = books.findById(id);
+        model.addAttribute("bookEdit",bookEdit);
+        return "editBook";
+    }
+
+    @PutMapping("/books/{id}/update")
+    public String editBook(@Valid @ModelAttribute("bookEdit") Book bookEdit, BindingResult result, Model model, @PathVariable("id") Long id, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        User userLogged = users.findById(userId);
+        if (result.hasErrors()) {
+            return "editBook";
+        } else {
+            bookEdit.setUser(userLogged);
+            books.create(bookEdit);
+            return "redirect:/home";
+        }
+    }
+
+    @DeleteMapping("/books/{id}/delete")
+    public String deleteBook(@PathVariable("id") Long id, HttpSession session) {
+        books.deleteBook(id);
+        return "redirect:/home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        return "redirect:/";
+    }
+}
